c6480c42f398a9340935dd7d5b32c6628608171c
er.musthofa <er.musthofa@gmail.com>
2020-05-30 20:59:33 +0700

    [frontend] update metadata

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -4,7 +4,7 @@ import '../style/styles.css'
 function Footer() {
   return (
     <div className="copyright py-4 text-center text-white">
-      <div className="container"><small>Copyright © Trivia Date 2020</small></div>
+      <div className="container"><small>Copyright © 2020 Random Name</small></div>
     </div>
   )
 }
